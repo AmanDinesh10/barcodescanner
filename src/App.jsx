f2a0d70d72326cffb2ea8a599fa7b77b97a3f049
AmanDinesh <xarmaaaruti@gmail.com>
2025-04-06 09:26:11 +0530

    Added both

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 // import CameraOCR from './CameraOCR'
 // import Scanner from './Scanner'
-// import SmartScanner from './SmartScanner'
+import SmartScanner from "./SmartScanner";
 // import HtmlQrCode from './HtmlQrCode'
-import BarcodeScanner from './BarcodeScanner'
+import BarcodeScanner from "./BarcodeScanner";
 
 function App() {
+  const [showBarCodeScanner, setShowBarCodeScanner] = useState(true);
 
   return (
     <>
       <div>
-      {/* <Scanner />    */}
+        {/* <Scanner />    */}
 
-      {/* <CameraOCR /> */}
-      {/* <SmartScanner /> */}
-      {/* <HtmlQrCode /> */}
-      <BarcodeScanner />
+        {/* <CameraOCR /> */}
+        {/* <SmartScanner /> */}
+        {/* <HtmlQrCode /> */}
+
+        <button onClick={() => setShowBarCodeScanner((prev) => !prev)}>
+          {!showBarCodeScanner ? "Open BarCodeScanner" : "Open SmartScanner"}
+        </button>
+
+        {showBarCodeScanner ? <BarcodeScanner /> : <SmartScanner />}
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
